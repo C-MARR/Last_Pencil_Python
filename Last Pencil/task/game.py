ce42f91e90733380b2a09b6b063b9c88eaade1ee
Cameron <c-em@outlook.com>
2022-09-23 09:02:54 -0700

    Added checks for correct pencil amounts and name selection

--- Last Pencil/task/game.py
+++ Last Pencil/task/game.py
@@ -1,10 +1,18 @@
 print("How many pencils would you like to use:")
-pencils = int(input())
+pencils = input()
+while not pencils.isnumeric() or pencils == "0":
+    if pencils == "0":
+        pencils = input("The number of pencils should be positive\n")
+    else:
+        pencils = input("The number of pencils should be numeric\n")
+pencils = int(pencils)
 first_name = "Cameron"
 second_name = "Sarah"
 print(f"Who will be the first ({first_name}, {second_name}):")
 first = input()
 first_name_turn = True
+while first != first_name and first != second_name:
+    first = input(f"Choose between {first_name} and {second_name}\n")
 if first == second_name:
     first_name_turn = False
 
@@ -16,3 +24,4 @@ while pencils > 0:
         print(f"{second_name}'s turn:")
     pencils -= int(input())
     first_name_turn = not first_name_turn
+
